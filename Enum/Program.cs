37bf6ad4878dc1430128ae726dc5fe3d354193bb
cemaldenizulucak <39682041+cemaldenizulucak@users.noreply.github.com>
2022-04-18 16:03:25 +0300

    Enum
    Enum Kullanımı

---
+++ Enum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aldığı değeri ekrana vermek için int ekledik.
+            Console.WriteLine((int)Kategoriler.Elektronik);
+        }
+
+
+        // Önemli bir yeri vardır çünkü tekrar yazımları kodları engeller hız ve verim kazandırır.
+        enum Kategoriler
+        {
+            Gıda = 1,
+            Hırdavat = 2,
+            Elektronik = 3,
+            Güzellik = 4,
+            Giyim = 5      
+
+        }
+    }
+}
